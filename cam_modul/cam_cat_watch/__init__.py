f8d45aec237e1df0dc7ebf1cf2a63be74f2f2a90
radianer <radianer@gmail.com>
2021-02-28 10:49:30 +0100

    change to mqtt

--- cam_modul/cam_cat_watch/__init__.py
+++ cam_modul/cam_cat_watch/__init__.py
@@ -3,50 +3,35 @@
 """
 import cv2 as cv
 import threading
-
-
-LOCK = threading.Lock()
+import paho.mqtt.client as mqtt
+import base64
 
 class CamCatWatch:
     
-    def __init__(self, cam_id):
+    def __init__(self, cam_id, resolution, mqtt_broker, mqtt_topic):
         self.cam_id = cam_id
+        self.resolution = resolution
+        self.mqtt_broker = mqtt_broker
+        self.mqtt_topic = mqtt_topic
         self.running = True
-        self.output_frame = None
-        self.t = threading.Thread(target=self.loop, args=())
-        self.t.daemon = True
-        self.t.start()
-        pass
 
-    def loop(self):
-        global LOCK
+    def start(self):
+        client = mqtt.Client()
+        client.connect(self.mqtt_broker)
+
         cap = cv.VideoCapture(self.cam_id)
         cap.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-        cap.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
-        cap.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
+        cap.set(cv.CAP_PROP_FRAME_WIDTH, self.resolution[0])
+        cap.set(cv.CAP_PROP_FRAME_HEIGHT, self.resolution[1])
         
         while self.running:
             ret, frame = cap.read()
             if not ret:
-                print("Fail to read frame")
+                print("Fail to read")
                 continue
-
-            with LOCK:
-                self.output_frame = frame.copy()
-
-    def gen_frames(self):
-        global LOCK
-
-        while self.running:
-            with LOCK:
-                if self.output_frame is None:
-                    continue
-
-                ret, buffer = cv.imencode(".jpg", self.output_frame)
-                if not ret:
-                    print("Failt to encode")
-                    continue
-                frame = buffer.tobytes()
-                yield(b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        pass
+            ret, buffer = cv.imencode(".jpg", frame)
+            if not ret:
+                print("Fail to encode")
+            
+            jpg_base = base64.b64encode(buffer)
+            client.publish(self.mqtt_topic, jpg_base)
--- cam_modul/main.py
+++ cam_modul/main.py
@@ -7,9 +7,10 @@ import cv2 as cv
 
 CAM_ID = 0
 FRAME_RESOLUTION = [1920, 1080]
+MQTT_BROKER = "192.168.178.27"
+MQTT_TOPIC = "front/door/cam"
 
 if __name__ == "__main__":
     print("start")
-    cat_watcher = CamCatWatch(CAM_ID)
-    server = CamCatServer(cat_watcher)
-    server.run()
+    cat_watcher = CamCatWatch(CAM_ID, FRAME_RESOLUTION, MQTT_BROKER, MQTT_TOPIC)
+    cat_watcher.start()
