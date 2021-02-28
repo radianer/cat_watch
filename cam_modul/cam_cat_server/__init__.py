042720279f3db25f06a79ae0e4a559f132b0a6c8
Radianer <radianer@gmail.com>
2021-02-28 07:49:48 +0100

    set cam props

--- cam_modul/cam_cat_server/__init__.py
+++ cam_modul/cam_cat_server/__init__.py
@@ -9,13 +9,13 @@ class CamCatServer:
         self.cat_watcher = cat_watcher
         self.app = Flask(__name__)
 
-        # @self.app.route('/')
-        # def index():
-        #     return render_template("index.html")
+        @self.app.route('/')
+        def index():
+            return render_template("index.html")
     
-        # @self.app.route("/video_feed")
-        # def video_feed():
-        #     return Response(self.cat_watcher.gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        @self.app.route("/video_feed")
+        def video_feed():
+            return Response(self.cat_watcher.gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
     
     def run(self):
         self.app.run(debug=True)
