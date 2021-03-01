e022dd0f8b36c524f9159c9bb8960deeee5d61f2
radianer <radianer@gmail.com>
2021-03-01 18:41:46 +0100

    add restart

--- cam_modul/main.py
+++ cam_modul/main.py
@@ -3,6 +3,7 @@
 """
 from cam_cat_watch import CamCatWatch
 import cv2 as cv
+import sys
 
 CAM_ID = 0
 FRAME_RESOLUTION = [1920, 1080]
@@ -10,6 +11,11 @@ MQTT_BROKER = "192.168.178.27"
 MIN_AREA = 200
 
 if __name__ == "__main__":
-    print("start")
-    cat_watcher = CamCatWatch(CAM_ID, FRAME_RESOLUTION, MQTT_BROKER, MIN_AREA)
-    cat_watcher.start()
+    while True:
+        try:
+            print("start")
+            cat_watcher = CamCatWatch(CAM_ID, FRAME_RESOLUTION, MQTT_BROKER, MIN_AREA)
+            cat_watcher.start()
+        except:
+            print("Exception")
+            print(sys.exc_info()[0])
