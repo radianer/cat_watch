9805171a268b8776feade0aad0468bf5a1ee603d
Radianer <radianer@gmail.com>
2021-03-25 16:35:39 +0100

    tcp server

--- cam_server/main.py
+++ cam_server/main.py
@@ -1,7 +1,7 @@
 from server import Server
 
-input_port = 8080
-output_port = 8081
+input_port = 9090
+output_port = 9091
 
 if __name__ == "__main__":
     server = Server(input_port, output_port)
--- cam_server/server/__init__.py
+++ cam_server/server/__init__.py
@@ -11,12 +11,18 @@ class Server:
         self.output_server = None
         self.hostname = socket.gethostname()
         self.running = True
+        
+        self.input_serve_th = None
+        self.output_serve_th = None
 
     def start(self):
         print("Server started")
         self.input_server = socketserver.TCPServer((self.hostname, self.input_port), TCPInputHandler)
         self.output_server = socketserver.TCPServer((self.hostname, self.output_port), TCPOutputHandler)
-        self.input_server.serve
+        self.input_serve_th = threading.Thread(target=self.input_server.serve_forever)
+        self.output_serve_th = threading.Thread(target=self.output_server.serve_forever)
+        for t in self.input_serve_th, self.output_serve_th: t.start()
+        for t in self.input_serve_th, self.output_serve_th: t.join()
         
 
 class TCPInputHandler(socketserver.BaseRequestHandler):
